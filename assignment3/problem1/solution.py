2039138c518843c3e996b3d457b97e80a4a091e7
cjf5729 <72352792+cjf5729@users.noreply.github.com>
2020-10-13 14:08:34 -0400

    Add files via upload

--- assignment3/problem1/solution.py
+++ assignment3/problem1/solution.py
@@ -6,41 +6,40 @@
 # Agha Arib Hyder; aah5469@psu.edu
 from typing import List
 
+
+# Declare global variables.
 num_connected = 0
-current_clock = 0
 post_list: List[int] = []
 visited_vertices = []
 
 
-def dfs_with_timing(adjacency_list, num_vert, explore_order):
+# 
+def dfs(adjacency_list, num_vert, explore_order):
     global visited_vertices
-    global current_clock
     global num_connected
+    global post_list
     num_connected = 0
-    current_clock = 0
     visited_vertices = [-1] * num_vert
     post_list = []
+    
     for vertex in explore_order:
-        print("testing  vertex ", vertex, "edges are ", adjacency_list[vertex])
         if visited_vertices[vertex] == -1:
             num_connected += 1
-            print("connected: ",num_connected)
             explore(adjacency_list, vertex, num_vert)
     return
 
 
 def explore(adjacency_list, vertex, num_vert):
-    global current_clock
     global post_list
     global num_connected
-    print("exploring vertice: ", vertex, " at clock: ", current_clock)
+    global visited_vertices
     visited_vertices[vertex] = num_connected
-    print(visited_vertices)
+    
     for edge in adjacency_list[vertex]:
         if visited_vertices[edge] == -1:
             explore(adjacency_list, edge, num_vert)
+    
     post_list.append(vertex)
-    current_clock += 1
     return
 
 
@@ -50,6 +49,7 @@ def get_post_list():
 
 def get_connected():
     return num_connected
+
 
 inp_list = [int(vertex) for vertex in input().split(" ") if vertex.isdigit()]
 
@@ -68,11 +68,10 @@ for edge in range(numE):
         reverse_adjacency_list[vertex2-1] = [vertex1-1]
     else:
         reverse_adjacency_list[vertex2-1].append(vertex1-1)
-dfs_with_timing(reverse_adjacency_list, numV, range(numV))
-post_order = get_post_list()
-print("The post list should be ", post_order)
-post_order.reverse()
-print("The reverse post list should be ", post_order)
 
-dfs_with_timing(adjacency_list, numV, post_order)
+dfs(reverse_adjacency_list, numV, range(numV))
+post_order = get_post_list()
+post_order.reverse()
+
+dfs(adjacency_list, numV, post_order)
 print(get_connected())
